bc6e7b143eda1220d29fa09b895cace0267a7632
gokuthug1 <102106500+gokuthug1@users.noreply.github.com>
2025-03-10 22:41:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ let musicList = [
   { name: "Genocide", url: "https://audio.jukehost.co.uk/b9M5F2nZPE7q4bJXVOXuAspE40R1qEvX" },
   { name: "Bloodshed", url: "https://audio.jukehost.co.uk/hfm6sZitk3798iMlizdfOqpQO5jFSZ8j" },
   { name: "Drawn to the Bitter", url: "https://audio.jukehost.co.uk/VsVWpw74dT7dB6IwspEMGbBslICNgyOa" },
-  { name: "Just Gold", url: "https://audio.jukehost.co.uk/L3D5OmmR4DbWOyTGS54MMgql2AAK6vl5" }
+  { name: "Just Gold", url: "https://audio.jukehost.co.uk/L3D5OmmR4DbWOyTGS54MMgql2AAK6vl5" },
+  { name: "Join us for a bite", url: "https://audio.jukehost.co.uk/zMMQkpi7Gq1vQU97Fyo5CQGbwfyFo15C"
 ];
 let currentSongIndex = 0;
 let audioPlayer = new Audio();
